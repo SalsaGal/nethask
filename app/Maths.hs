cff9b3ce6c08865639cc1c7baef4a535e993937e
SalsaGal <fielddashos@gmail.com>
2023-03-23 16:34:14 +1100

    Add add and sub to Vec2

--- app/Maths.hs
+++ app/Maths.hs
@@ -1,3 +1,9 @@
 module Maths where
 
 data Vec2 a = Vec2 {x :: a, y :: a} deriving Show
+
+(+) :: (Num a) => Vec2 a -> Vec2 a -> Vec2 a
+(+) a b = Vec2 { x=x a Prelude.+ x b, y=y a Prelude.+ y b }
+
+(-) :: (Num a) => Vec2 a -> Vec2 a -> Vec2 a
+(-) a b = Vec2 { x=x a Prelude.- x b, y=y a Prelude.- y b }
